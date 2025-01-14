0cba11808cc7054d8b9b790a65fd8f236b367d7c
Chantorn1986 <p.chantorn1986@gmail.com>
2025-01-14 15:41:17 +0700

    add some fretures

--- app.js
+++ app.js
@@ -56,70 +56,7 @@ app.set('view engine', 'ejs');
 
 
 app.use('/',require('./routes/pages'));
-/*
-app.get('/', (req, res) => {
-    const sql = "SELECT * FROM products";
 
-    db.query(sql, (err, results) => {
-        if (err) throw err;
-
-        res.render('home', { 
-            title: 'Home',
-            products: results
-        });
-    })
-});
-
-app.get('/create', (req, res) => {
-    res.render('create');
-})
-
-app.post('/create', upload.single('image'), (req, res) => {
-    const { name, description } = req.body;
-    const image = req.file ? req.file.filename : null;
-
-    const sql = "INSERT INTO products (name, description, image) VALUES(?, ?, ?)";
-    db.query(sql, [name, description, image], (err, result) => {
-        if (err) throw err;
-        res.redirect('/');
-    })
-})
-
-app.get('/edit/:id', (req, res) => {
-    const sql = "SELECT * FROM products WHERE id = ?";
-    db.query(sql, [req.params.id], (err, result) => {
-        if (err) throw err;
-        res.render('edit', { product: result[0] });
-    });
-})
-
-app.post('/edit/:id', upload.single('image'), (req, res) => {
-    const { name, description } = req.body;
-    const image = req.file ? req.file.filename : req.body.oldImage;
-
-    const sql = "UPDATE products SET name = ?, description = ?, image = ? WHERE id = ?";
-    db.query(sql, [name, description, image, req.params.id], (err, result) => {
-        if (err) throw err;
-        res.redirect('/');
-    })
-})
-
-app.get('/delete/:id', (req, res) => {
-    const sql = "DELETE FROM products WHERE id = ?";
-    db.query(sql, [req.params.id], (err, result) => {
-        if (err) throw err;
-        res.redirect('/');
-    });
-})
-
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'About'});
-});
-
-app.get('/contact', (req, res) => {
-    res.render('contact', { title: 'Contact'});
-});
-*/
 app.listen(3000, () => {
     console.log("Server is running...");
 });
